e40cec92201f784b8a66b716223214968db8a786
mikeaussie <michael.pyatin@me.com>
2016-06-25 12:14:04 +1000

    graphic interface

---
+++ Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  aurora-firebase
+//
+//  Created by Mike Piatin on 25/06/2016.
+//  Copyright © 2016 Aurora Software. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0/255.0 //the color value must be divided by 255.0
+
+//Keys
+let KEY_UID = "uid"
+
+//Segues
+let SEGUE_LOGGED_IN = "loggedIn"
+
+//Status codes
+let STATUS_ACCOUNT_NONEXIST = -8
